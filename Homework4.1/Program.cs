da12700187f46887cd864e69e4a941a9a497ec27
Danylo <Danylo@DESKTOP-JA5F15Q>
2022-11-26 19:51:17 +0200

    Add project files.

---
+++ Homework4.1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+interface IGeographicalObject
+{
+    string GetInfo();
+}
+
+class River : IGeographicalObject
+{
+    private int x;
+    public int X { get { return x; } set { x = value; } }
+    
+    private int y;
+    public int Y { get { return y; } set { y = value; } }
+    
+    private string name = "";
+    public string Name { get { return name; } set { name = value; } }
+   
+    private string description = "";
+    public string Description { get { return description; } set { description = value; } }
+
+    private int speedInMPS;
+    public int SpeedInMPS { get { return speedInMPS; } set {speedInMPS = value; } }
+    
+    private int len;
+    public int Len { get { return len; } set { len = value; } }
+
+    public string GetInfo()
+    {
+        string output = String.Format("X: {0}, Y: {1}. Name: {2}, description: {3}. Speed in mps: {4}, length: {5}", x, y, name, description,
+            speedInMPS, len);
+        return output;
+
+    }
+}
+
+class Mountain : IGeographicalObject
+{
+    private int x;
+    public int X { get { return x; } set { x = value; } }
+
+    private int y;
+    public int Y { get { return y; } set { y = value; } }
+
+    private string name = "";
+    public string Name { get { return name; } set { name = value; } }
+
+    private string description = "";
+    public string Description { get { return description; } set { description = value; } }
+
+    private int[] highestPoint = { 0, 0 };
+    public int[] HighestPoint { get { return highestPoint; } set { highestPoint = value;} }
+
+    public string GetInfo()
+    {
+        string output = String.Format("X: {0}, Y: {1}. Name: {2}, description: {3}. Highest point: {4}", x, y, name, description,
+            highestPoint.ToString());
+        return output;
+
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
---
+++ ViaAbstractClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.Linq;
+
+abstract class GeographicalObject
+{
+    protected int x { get; set; }
+
+    protected int y { get; set; }
+
+    protected string name = "";
+    public string Name { get { return name; } set { name = value; } }
+
+    protected string description = "";
+    public string Description { get { return description; } set { description = value; } }
+    virtual public string GetInfo()
+    {
+        string output = String.Format("X: {0}, Y: {1}. Name: {2}, description: {3}", x, y, name, description);
+        return output;
+    }
+
+}
+
+class River : GeographicalObject
+{
+    private int speedInMPS { get; set; }
+    private int len { get; set; }
+    public override string GetInfo()
+    {
+        string output = String.Format("X: {0}, Y: {1}. Name: {2}, description: {3}. Speed in mps: {4}, length: {5}", x, y, name, description,
+            speedInMPS, len);
+        return output;
+    }
+}
+
+class Mountain : GeographicalObject
+{
+    private int[] highestPoint = { 0, 0 };
+    public int[] HighestPoint { get { return highestPoint; } set { highestPoint = value; } }
+    public override string GetInfo()
+    {
+        string output = String.Format("X: {0}, Y: {1}. Name: {2}, description: {3}. Highest point: {4}", x, y, name, description,
+            highestPoint.ToString());
+        return output;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
